f44e08515238f34da810e58c92440373f7d38b43
Stavsavv <zapat1Laa@hotmail.com>
2024-11-04 15:45:50 +0200

    Update Navigation.js

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -7,25 +7,25 @@ function Nav() {
         {/* User information */}
         <div className="flex items-center gap-2">
           {/* img */}
-          <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+          <div className="h-[80px] w-[80px] rounded-full overflow-hidden">
             <img
               className="object-cover w-full h-full"
-              src="https://thispersondoesnotexist.com/image"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvi7HpQ-_PMSMOFrj1hwjp6LDcI-jm3Ro0Xw&s"
               alt="Profile image"
             />
           </div>
 
           {/* name */}
-          <small>Hi, Leon!</small>
+          <big className="font-bold">Hi, Stavros!</big>
         </div>
 
         {/* Right side of our navigation */}
         <nav className="flex items-center gap-4">
           <div>
-            <ImStatsBars className="text-2xl" />
+            <ImStatsBars className="text-4xl" />
           </div>
           <div>
-            <button className="btn btn-danger">Sign out</button>
+            <button className="btn btn-danger text-12xl">Sign out</button>
           </div>
         </nav>
       </div>
